66f8e9689f5c3260b08deba4e7b6f10c9e1b3a1b
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-06-11 12:19:22 -0500

    chore: first commit

---
+++ src/main/java/quri/teelab/api/teelab/TeeLabApplication.java
@@ -0,0 +1,13 @@
+package quri.teelab.api.teelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeeLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeeLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/quri/teelab/api/teelab/TeeLabApplicationTests.java
@@ -0,0 +1,13 @@
+package quri.teelab.api.teelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeeLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
