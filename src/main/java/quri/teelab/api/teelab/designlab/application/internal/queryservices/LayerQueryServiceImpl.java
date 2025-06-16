52b82d70ed0b70495e277b06e389093800772f18
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-06-16 11:29:25 -0500

    feat: add ProjectController and implement ProjectQueryService and LayerQueryService

---
+++ src/main/java/quri/teelab/api/teelab/designlab/application/internal/queryservices/LayerQueryServiceImpl.java
@@ -0,0 +1,8 @@
+package quri.teelab.api.teelab.designlab.application.internal.queryservices;
+
+import org.springframework.stereotype.Service;
+import quri.teelab.api.teelab.designlab.domain.services.LayerQueryService;
+
+@Service
+public class LayerQueryServiceImpl implements LayerQueryService {
+}
---
+++ src/main/java/quri/teelab/api/teelab/designlab/application/internal/queryservices/ProjectQueryServiceImpl.java
@@ -0,0 +1,8 @@
+package quri.teelab.api.teelab.designlab.application.internal.queryservices;
+
+import org.springframework.stereotype.Service;
+import quri.teelab.api.teelab.designlab.domain.services.ProjectQueryService;
+
+@Service
+public class ProjectQueryServiceImpl implements ProjectQueryService {
+}
