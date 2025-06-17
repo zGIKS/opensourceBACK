3283be76a3c62d5c5af9a5070de9456462035ffc
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-06-17 00:52:02 -0500

    feat: add fulfillment infrastructure layer

---
+++ src/main/java/quri/teelab/api/teelab/orderfulfillment/infrastructure/persistence/jpa/repositories/FulfillmentRepository.java
@@ -0,0 +1,9 @@
+package quri.teelab.api.teelab.orderfulfillment.infrastructure.persistence.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import quri.teelab.api.teelab.orderfulfillment.domain.model.aggregates.Fulfillment;
+
+@Repository
+public interface FulfillmentRepository extends JpaRepository<Fulfillment, Long> {
+}
