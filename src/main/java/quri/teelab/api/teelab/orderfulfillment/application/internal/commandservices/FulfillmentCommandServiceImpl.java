1b9e662aa5be20e9cb458a581030554d355c1b5e
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-06-17 02:20:24 -0500

    feat: update fulfillment application layer

---
+++ src/main/java/quri/teelab/api/teelab/orderfulfillment/application/internal/commandservices/FulfillmentCommandServiceImpl.java
@@ -0,0 +1,24 @@
+package quri.teelab.api.teelab.orderfulfillment.application.internal.commandservices;
+
+import org.springframework.stereotype.Service;
+import quri.teelab.api.teelab.orderfulfillment.domain.model.aggregates.Fulfillment;
+import quri.teelab.api.teelab.orderfulfillment.domain.model.commands.CreateFulfillmentCommand;
+import quri.teelab.api.teelab.orderfulfillment.domain.services.FulfillmentCommandService;
+import quri.teelab.api.teelab.orderfulfillment.infrastructure.persistence.jpa.repositories.FulfillmentRepository;
+
+@Service
+public class FulfillmentCommandServiceImpl implements FulfillmentCommandService {
+    
+    private final FulfillmentRepository fulfillmentRepository;
+    
+    public FulfillmentCommandServiceImpl(FulfillmentRepository fulfillmentRepository) {
+        this.fulfillmentRepository = fulfillmentRepository;
+    }
+      @Override
+    public Long handle(CreateFulfillmentCommand command) {
+        var fulfillment = new Fulfillment(command);
+        
+        fulfillmentRepository.save(fulfillment);
+        return fulfillment.getId();
+    }
+}
