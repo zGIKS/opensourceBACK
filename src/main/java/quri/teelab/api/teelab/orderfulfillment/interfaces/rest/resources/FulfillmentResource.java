20713f5c1f2c74acdec3bd15ba250bf4608ce90c
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-06-17 17:04:55 -0500

    refactor: change status from String to Enum variable.

--- src/main/java/quri/teelab/api/teelab/orderfulfillment/interfaces/rest/resources/FulfillmentResource.java
+++ src/main/java/quri/teelab/api/teelab/orderfulfillment/interfaces/rest/resources/FulfillmentResource.java
@@ -1,11 +1,12 @@
 package quri.teelab.api.teelab.orderfulfillment.interfaces.rest.resources;
 
+import quri.teelab.api.teelab.orderfulfillment.domain.model.valueobjects.FulfillmentStatus;
 import java.util.Date;
 
 public record FulfillmentResource(
         String id,
         String orderId,
-        String status,
+        FulfillmentStatus status,
         Date receivedDate,
         Date shippedDate,
         String manufacturerId,
