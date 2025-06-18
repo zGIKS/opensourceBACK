207ac0690bdf1346c0c3ccfcf1b663074c9034cb
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-06-18 16:27:25 -0500

    refactor: enhance entity classes with JPA annotations and constructors for better data handling

--- src/main/java/quri/teelab/api/teelab/designlab/domain/model/entities/ImageLayer.java
+++ src/main/java/quri/teelab/api/teelab/designlab/domain/model/entities/ImageLayer.java
@@ -1,21 +1,32 @@
 package quri.teelab.api.teelab.designlab.domain.model.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import org.hibernate.validator.constraints.URL;
+import quri.teelab.api.teelab.designlab.domain.model.valueobjects.LayerId;
+import quri.teelab.api.teelab.designlab.domain.model.valueobjects.LayerType;
 
 @Entity
 @DiscriminatorValue("IMAGE")
 @Getter
 public class ImageLayer extends Layer {
-    @Getter
     @URL
+    @Column(nullable = false)
     private String imageUrl;
-
-    @Getter
     private Float width;
-
-    @Getter
     private Float height;
+
+    protected ImageLayer() {}
+
+    public ImageLayer(
+            LayerId id, int x, int y, int z, Float opacity, boolean isVisible, LayerType type,
+            String imageUrl, Float width, Float height
+    ) {
+        super(id, x, y, z, opacity, isVisible, type);
+        this.imageUrl = imageUrl;
+        this.width = width;
+        this.height = height;
+    }
 }
--- src/main/java/quri/teelab/api/teelab/designlab/domain/model/entities/Layer.java
+++ src/main/java/quri/teelab/api/teelab/designlab/domain/model/entities/Layer.java
@@ -11,24 +11,47 @@ import java.util.Date;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "layer_type", discriminatorType = DiscriminatorType.STRING)
 @Getter
-public class Layer {
+public abstract class Layer {
     @EmbeddedId
     private LayerId id;
 
+    @Column(nullable = false)
     private int x;
+    @Column(nullable = false)
     private int y;
+    @Column(nullable = false)
     private int z;
 
+    @Column(nullable = false)
     private Float opacity;
+    @Column(nullable = false)
     private boolean isVisible;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private LayerType type;
 
     @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
     private Date createdAt;
 
     @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
     private Date updatedAt;
 
+    protected Layer() {}
+
+    public Layer(LayerId id, int x, int y, int z, Float opacity, boolean isVisible, LayerType type) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.opacity = opacity;
+        this.isVisible = isVisible;
+        this.type = type;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+
+    public LayerId getId() { return id; }
 }
